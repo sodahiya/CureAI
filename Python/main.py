b0d5209d4f7801788120744f5899b5a2ad1bc226
Sodahiya <Sodahiya@gmail.com>
2025-04-13 12:38:36 +0530

    modified python code

--- Python/main.py
+++ Python/main.py
@@ -10,10 +10,10 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Your Next.js dev server
+    allow_origins=["*"],  # Allows all origins
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
 )
 
 # Initialize Ollama client once
